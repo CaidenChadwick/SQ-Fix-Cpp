86d4da04db2a5039a51dc7574fe631099608200f
Caiden Chadwick <91963937+CaidenChadwick@users.noreply.github.com>
2025-12-01 08:15:22 -0600

    Add files via upload

--- swig_toctou_unsafe.cpp
+++ swig_toctou_unsafe.cpp
@@ -10,19 +10,15 @@ int create_cachedirtag() {
     struct stat st;
 
     // Check if file already exists
-    if (stat(filename.c_str(), &st) == 0) {
-        errno = EEXIST;
-        return -1;
+    if (stat(filename.c_str(), &st) != 0) {
+        // Race window exists here: another process can create/replace the file now
+        FILE *f = fopen(filename.c_str(), "w");  // <-- SonarQube should flag this
+        if (f == nullptr) {
+            return -1;
+        }
+        fclose(f);
     }
-
-    // Race window exists here: another process can create/replace the file now
-
-    FILE *f = fopen(filename.c_str(), "w");  // <-- SonarQube should flag this
-    if (f == nullptr) {
-        return -1;
-    }
-
-    fclose(f);
+    
     return 0;
 }
 
